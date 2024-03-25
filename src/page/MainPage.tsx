a9db15363bf0a9716c7a7f12f95eff4b8f40ecb4
Mulmer <mulmer663@gmail.com>
2024-03-25 23:39:45 +0900

    로고 추가

---
+++ src/page/MainPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Logo from "../component/header/Logo";
+
+const MainPage = () => {
+    return (
+        <div className="max-w-screen-sm container mx-auto">
+            <Logo/>
+            <div>tab</div>
+            <div></div>
+        </div>
+    );
+};
+
+export default MainPage;
