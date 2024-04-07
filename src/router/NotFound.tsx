8ba1c6bd268e7b727dbd29943f6a80631cf4a261
Mulmer <mulmer663@gmail.com>
2024-04-07 18:14:47 +0900

    라우터 404 페이지 수정

--- src/router/NotFound.tsx
+++ src/router/NotFound.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 /**
  * 페이지 없을 때 나오는 404 에러페이지
  */
 const NotFound = () => {
     return (
-        <div className="flex min-h-screen flex-grow items-center justify-center bg-gray-50">
+        <div className="flex h-full flex-grow items-center justify-center bg-gray-50">
             <div className="rounded-lg bg-white p-8 text-center shadow-xl">
                 <h1 className="mb-4 text-4xl font-bold font-gmarket">404</h1>
                 <p className="text-gray-600 font-noto-sans">Oops! The page you are looking for could not be found.</p>
-                <a href="/"
+                <Link to="/"
                    className="mt-4 inline-block rounded bg-orange-500 px-4 py-2 font-semibold text-white font-noto-sans hover:bg-orange-600"> Go
-                    back to Home </a>
+                    back to Home </Link>
             </div>
         </div>
     );
